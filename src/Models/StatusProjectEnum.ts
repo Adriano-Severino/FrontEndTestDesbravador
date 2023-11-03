e942a6fc2a694fa5edf425fad4206236a4a3253f
Adriano-Severino <adriano.x1@hotmail.com>
2023-11-02 23:10:18 -0300

    editado enum

--- src/Models/StatusProjectEnum.ts
+++ src/Models/StatusProjectEnum.ts
@@ -1,11 +1,12 @@
 export enum StatusProjectEnum {
-    'Em análise' = 0,
-    'Análise realizada' = 1,
-    'Análise aprovada' = 2,
-    'Iniciado' = 3,
-    'Planejado' = 4,
-    'Em andamento' = 5,
-    'Encerrado' = 6,
-    'Cancelado' = 7
+    'Em criação' = 0,
+    'Em análise' = 2,
+    'Análise realizada' = 4,
+    'Análise aprovada' = 6,
+    'Iniciado' = 8,
+    'Planejado' = 10,
+    'Em andamento' = 12,
+    'Encerrado' = 14,
+    'Cancelado' = 16
   
   }
